5826ff2a2d55b26278dcd78cd674fbda466c3494
linhdev99 <linh.huynh.dev.99@gmail.com>
2019-12-11 00:16:12 +0700

    linh xong roiiii

--- pl/total_pl.sql
+++ pl/total_pl.sql
@@ -315,8 +315,70 @@ BEGIN
 			print 'Incorrect current password' 
 		END
 END
+GO
+CREATE FUNCTION funcAvgRate (@idShop VARCHAR(6))
+RETURNS FLOAT
+AS
+BEGIN
+	DECLARE @avgRate AS FLOAT
+	SET @avgRate = 0
+	--SET @avgRate = (SELECT AVG(CAST(star AS FLOAT)) FROM tblRate WHERE idShop = @idShop)
+	DECLARE @tempTable TABLE(idShop VARCHAR(6), idCustomer VARCHAR(6), star INT, describe NVARCHAR(100))
+	INSERT INTO @tempTable (idShop, idCustomer, star, describe) SELECT idShop, idCustomer, star, describe FROM dbo.tblRate WHERE idShop = @idShop
+	DECLARE @count FLOAT = (SELECT COUNT(idShop) FROM @tempTable WHERE idShop = @idShop)
+	WHILE (SELECT COUNT(*) FROM @tempTable WHERE idShop = @idShop) > 0
+		BEGIN
+		SET @avgRate = @avgRate + (SELECT TOP 1 star FROM @tempTable WHERE idShop = @idShop)
+		UPDATE TOP (1) @tempTable Set idShop = 'x' Where idShop = @idShop 
+		END
+	SET @avgRate = @avgRate/@count
+	RETURN @avgRate
+END
+GO
+SELECT * FROM dbo.tblRate
+SELECT dbo.[funcAvgRate]('CH0001')
+GO
+CREATE FUNCTION funcFindRate(@idShop VARCHAR(6), @star INT, @type INT) --type = 0 --> "=", type = 1 --> "<=", type = 2 --> ">="
+RETURNS @tempTable TABLE (idShop VARCHAR(6), idCustomer VARCHAR(6), star INT, describe NVARCHAR(100))
+AS
+BEGIN
+	IF @type = 0
+	BEGIN
+		INSERT INTO @tempTable (idShop, idCustomer, star, describe) SELECT idShop, idCustomer, star, describe 
+		FROM dbo.tblRate WHERE idShop = @idShop AND star = @star
+	END
+	ELSE 
+		BEGIN
+			IF @type = 1
+			BEGIN
+				INSERT INTO @tempTable (idShop, idCustomer, star, describe) SELECT idShop, idCustomer, star, describe 
+				FROM dbo.tblRate WHERE idShop = @idShop AND star <= @star
+			END
+			ELSE
+			BEGIN
+				INSERT INTO @tempTable (idShop, idCustomer, star, describe) SELECT idShop, idCustomer, star, describe 
+				FROM dbo.tblRate WHERE idShop = @idShop AND star >= @star
+			END
+		END
+	RETURN
+END
+GO
+SELECT * FROM dbo.tblRate
+SELECT * FROM dbo.[funcFindRate]('CH0001',5,1)
 
+/*
+Declare @Id int
 
+While (Select Count(*) From ATable Where Processed = 0) > 0
+Begin
+    Select Top 1 @Id = Id From ATable Where Processed = 0
+
+    --Do some processing here
+
+    Update ATable Set Processed = 1 Where Id = @Id 
+
+End
+*/
 -- Phần của Liêm --
 GO
 CREATE TABLE tblTransportation
